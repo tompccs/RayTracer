564ac5429aca6fe66cecdc6fa5c91bbc9ad81e37
Mark Portnoi <misha@Mishas-MacBook-Pro.local>
2014-08-01 08:39:46 +0100

    Initial Commit

---
+++ RayTracer2.0/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RayTracer2.0
+//
+//  Created by Mark Portnoi on 01/08/2014.
+//  Copyright (c) 2014 Mark Portnoi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
