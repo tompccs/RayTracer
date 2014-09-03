369774bbf8f0c38d2ecb11fc4d9efd0260ca0624
Mark Portnoi <mark.portnoi.12@ucl.ac.uk>
2014-09-03 16:13:03 +0100

    MultipleObjects Class
    Added MultipleObjects Class. This contains methods for implementation
    of more than 1 LSC. These methods are not yet implemented, and need to
    be tested.

---
+++ RayTracer2.0/MultipleObjects.cpp
@@ -0,0 +1,52 @@
+#include "MultipleObjects.h"
+
+void
+MultipleObjects::StoreMaterial(Material* object){
+    ObjectList.push_back(object);
+}
+
+void
+MultipleObjects::StoreWorld(Material *WORLD){
+    world = WORLD;
+}
+
+void
+MultipleObjects::PrintSize(){
+    cout<<ObjectList.size()<<endl;
+}
+
+Material*&
+MultipleObjects::CurrentMaterial(){
+    for(int i = 0; i<ObjectList.size(); i++){
+        if(ObjectList[i]->ReturnPhotonInside())
+            return ObjectList[i];
+    }
+    return world;
+}
+
+bool
+MultipleObjects::PhotonInMaterial(){
+    for(int i = 0; i<ObjectList.size(); i++){
+        if(ObjectList[i]->ReturnPhotonInside())
+            return 1;
+    }
+    return 0;
+}
+
+double
+MultipleObjects::NextInterfaceDistance(Photon* photon){
+    vector<double> distances;
+    for(int i = 0; i<ObjectList.size(); i++){
+        distances.push_back(ObjectList[i]->GetInterfaceDistance(photon));
+    }
+    return calc.GetVectorMin(distances);
+}
+
+Material*&
+MultipleObjects::NextInterfaceMaterial(Photon *photon){
+    for(int i = 0; i<ObjectList.size(); i++){
+        if(ObjectList[i]->GetInterfaceDistance(photon)==NextInterfaceDistance(photon) && ObjectList[i]->ReturnPhotonInside()==0)
+            return ObjectList[i];
+    }
+    return world;
+}
---
+++ RayTracer2.0/MultipleObjects.h
@@ -0,0 +1,23 @@
+#ifndef __RayTracer__MultipleObjects__
+#define __RayTracer__MultipleObjects__
+
+#include <iostream>
+#include <vector>
+#include "Material.h"
+#include "Functions.h"
+
+class MultipleObjects{
+    vector<Material*>  ObjectList;
+    Material* world;
+    Functions calc;
+public:
+    void StoreMaterial(Material* object);
+    void StoreWorld(Material* WORLD);
+    void PrintSize();
+    Material*& CurrentMaterial();
+    bool PhotonInMaterial();
+    double NextInterfaceDistance(Photon* photon);
+    Material*& NextInterfaceMaterial(Photon* photon);
+};
+
+#endif
